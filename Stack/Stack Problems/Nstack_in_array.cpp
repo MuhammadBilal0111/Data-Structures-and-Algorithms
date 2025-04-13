67879d41ea701731ed25ed7e0d660575007f5d13
Muhammad Bilal <m.bilal0111@gmail.com>
2025-04-13 17:40:30 +0500

    Update Nstack_in_array.cpp

--- Stack/Stack Problems/Nstack_in_array.cpp
+++ Stack/Stack Problems/Nstack_in_array.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 class NStack
 {
     int *arr;
@@ -8,65 +9,60 @@ class NStack
 
     int n, s;
     int freespot;
+
+public:
     NStack(int N, int S)
     {
+        n = N;
+        s = S;
+
         arr = new int[S];
         top = new int[N];
         next = new int[S];
 
-        // initializing top
         for (int i = 0; i < n; i++)
         {
             top[i] = -1;
         }
-        // initializing next
+
         for (int i = 0; i < S; i++)
         {
             next[i] = i + 1;
         }
-        // update last index
+
         next[S - 1] = -1;
-        // update freespot
         freespot = 0;
     }
+
     bool push(int x, int m)
     {
         if (freespot == -1)
         {
             return false;
         }
+
         int index = freespot;
         freespot = next[index];
 
-        // insert element
         arr[index] = x;
-
-        // update next;
         next[index] = top[m - 1];
-
-        // update top
         top[m - 1] = index;
+
         return true;
     }
-    bool pop(int m)
+
+    int pop(int m)
     {
-        // check underflow condition
         if (top[m - 1] == -1)
         {
-            return -1;
+            return -1; // or throw exception
         }
 
         int index = top[m - 1];
-
         top[m - 1] = next[index];
-
         next[index] = freespot;
-
         freespot = index;
+
         return arr[index];
     }
 };
-int main()
-{
-    return 0;
-}
